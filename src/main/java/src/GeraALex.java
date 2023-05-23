713f700e4e2bc9434ae9c9293975d3c1a513bf27
JoseCarlos67 <carlos6767jose@gmail.com>
2023-05-23 17:08:37 -0300

    AnalisadorLexico, GeraALex, TesteALex, lex.flex

---
+++ src/main/java/src/GeraALex.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.nio.file.Paths;
+
+public class GeraALex {
+
+	public static void main(String[] args) {
+		
+		String rootPath = Paths.get("").toAbsolutePath().toString();
+		String subPath = "/src/main/java/src/";
+
+		String file[] = { rootPath + subPath + "lex.flex" };
+
+		jflex.Main.main(file);
+	}
+
+}
